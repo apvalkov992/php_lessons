5761142c8ef07cfac1778bf144ec16310b7aca19
tsirtsey <naskidashvilikirill@mail.ru>
2019-02-12 21:23:23 +0300

    #ISSUE-1# самостоятельная

--- homework/1/index.php
+++ homework/1/index.php
@@ -6,3 +6,21 @@
 $name = 'Kirill';
 
 var_dump($name);
+
+$NaUliceSneg = true;
+var_dump($NaUliceSneg);
+
+$MoiNomer = 245;
+var_dump($MoiNomer);
+
+$ChisloPi =3.14;
+var_dump($ChisloPi);
+
+$Zapyataya =67E-8;
+var_dump($Zapyataya);
+
+$MyName = 'Kirill';
+var_dump($MyName);
+
+$RabochiiKod = null;
+var_dump($RabochiiKod);
