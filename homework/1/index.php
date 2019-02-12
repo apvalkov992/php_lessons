390b00630e31efafd854610bb940c5dc77d4f765
mistiqueee <vasiliygvv@yandex.ru>
2019-02-12 23:00:29 +0300

    #ISSUE-1# изменение имени

--- homework/1/index.php
+++ homework/1/index.php
@@ -3,9 +3,16 @@
  *
  */
 
-$age = "34";
-$bool = "male";
-$name = 'Василий';
-$zero = "null";
+$name = "Василий"; //str = string
+$age = 34;	//Целое число
+$full_age =34.5; //Число с плавающей точкой
+$male = true; //Булево true|false
+$zero = null; //Null
 
-var_dump($name);
+echo "$name<br>";
+echo "$age<br>";
+echo "$full_age<br>";
+echo "$male<br>";
+
+?>
+
