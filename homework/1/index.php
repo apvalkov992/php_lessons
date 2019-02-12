5820394d330a2f40d2a2c40d5b38e10be6763d5b
superaft <ogula.kirill@gmail.com>
2019-02-12 21:26:52 +0300

    Создал переменные

--- homework/1/index.php
+++ homework/1/index.php
@@ -3,6 +3,24 @@
  *
  */
 
-$name = 'Kirill';
+$var_null = null;
+$var_bool = false;
+$var_int = 666;
+$var_float = 2.15;
+$var_string = "Kirill"; 
+$var_array = array(
+				   "login" => "superaft", 
+                   "branchName" => "KOgula", 
+                  );
 
-var_dump($name);
+var_dump($var_null);
+echo "<br>";
+var_dump($var_bool);
+echo "<br>";
+var_dump($var_int);
+echo "<br>";
+var_dump($var_float);
+echo "<br>";
+var_dump($var_string);
+echo "<br>";
+var_dump($var_array);
