e8bb1897c11625ac7c308cf6dfa740bc0903a180
UndergroundWizard <egor-kozh@bk.ru>
2019-02-12 22:43:05 +0300

    #ISSUE-1# Homework

--- homework/1/index.php
+++ homework/1/index.php
@@ -5,11 +5,25 @@
 
 $name = 'Egor';
 
-
-
-$int = '1';
-$bool = 'true';
-$real = '1.25';
+/*Self work */
+$int = 1;
+$bool = true;
+$real = 1.25;
 $string = 'String is here';
-$empty = 'NULL';
+$empty = NULL;
 
+var_dump($name);
+
+
+/* homework */
+
+
+$my_name = 'Egor';
+$egor = 'Kozhevnikov';
+define("AGE", 19);
+static $sex = male;
+global $CITY;
+$CITY = 'Donetsk';
+unset($my_name, $egor, $sex, $CITY);
+
+/*Переменные удалил, а константа осталась*/
