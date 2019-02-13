c0c3360ec0aba203be722f619d12e930f202abb3
tsirtsey <naskidashvilikirill@mail.ru>
2019-02-13 18:55:59 +0300

    #ISSUE-1# Домашнее задание

--- homework/1/index.php
+++ homework/1/index.php
@@ -16,8 +16,35 @@ var_dump($MoiNomer);
 $Zapyataya =67E-8;
 var_dump($Zapyataya);
 
-$MyName = 'Kirill';
-var_dump($MyName);
+$MySurname = 'Naskidashvili';
+var_dump($MySurname);
 
 $RabochiiKod = null;
-var_dump($RabochiiKod);
+
+// Домашнее задание
+
+$Gorod = 'Donetsk';
+
+$MoeImya = 'Kirill';
+
+$Kirill = 'Naskidashvili';
+
+define('AGE', 18);
+var_dump(AGE);
+
+function Zadanie()
+{
+	static $Gender = 'Male';
+}
+
+function City()
+{
+	global $Gorod;
+}
+City();
+echo $Gorod;
+
+unset($name,$NaUliceSneg, $MoiNomer, $Zapyataya, $MySurname, $RabochiiKod, $MoeImya, $Kirill, $Gender, $Gorod);
+
+echo $MoeImya;
+echo AGE;
