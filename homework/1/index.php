4be4324ae431e9e81d41ade0ddd52e54f1bb6799
Arkadiy4 <fffcs2@mail.ru>
2019-02-14 20:42:42 +0300

    #ISSUE-1# Домашняя работа 1

--- homework/1/index.php
+++ homework/1/index.php
@@ -2,12 +2,44 @@
 /**
  *
  */
+/* Самостоятельная работа доработанная*/
+$name = 'Arkadiy4';
+$nnn = true;
+$myfloat = 1.22;
+$pusto = null ;
 
-$name = 'Arkadiy';
-$Tovars = 100
-$Nali4ie = True 
-$decFloat = 1.22
-$ni4ego = NULL 
+var_dump($name, $nnn, $myfloat, $pusto);
+echo ("<br><br><br><br>");
 
 
-var_dump($name);
+
+/*Домашняя работа 1*/
+echo "  Homework #1 <br>";
+// Определение переменных
+$name = 'Arkadiy'; 		// Имя , строковая переменная
+$arkadiy = "Chekin";	// Фамилия , строковая переменная
+const AGE = 56;			// Возраст , константа , целая
+static $sex = "Male";	// Пол, статическа переменная(необнуляемая за областью видимости)
+global $city; $city = "Donetsk";			// город, глобальная переменная
+
+//Проверка переменных выводом на экран
+echo '<br>' ;
+echo 'name    = ', $name, '<br>';
+echo 'arkadiy = ', $arkadiy, '<br>';
+echo 'AGE     = ', AGE, "<br>";
+echo 'sex     = ', $sex, "<br>";
+echo 'city    = ', $city, "<br>";
+var_dump($name, $arkadiy, AGE, $sex, $city); 
+
+//Удаление переменных
+unset ($name, $arkadiy, $sex, $city);
+//runkit_constant_remove( "AGE") ; undefined function
+
+//Проверка переменных после удаления
+echo '<br><br>' ;
+echo 'name    = ', $name, '<br>';
+echo 'arkadiy = ', $arkadiy, '<br>';
+echo 'AGE     = ', AGE, "<br>";
+echo 'sex     = ', $sex, "<br>";
+echo 'city    = ', $city, "<br>";
+var_dump($name, $arkadiy, AGE, $sex, $city); 
