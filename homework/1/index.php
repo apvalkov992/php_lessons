38e6d8e1dee3b986af30912b63285d0cc1995dc4
Dima Duvanov <sdsdfhshs.dima@yandex.ru>
2019-02-15 18:16:32 +0300

    HomeWork

--- homework/1/index.php
+++ homework/1/index.php
@@ -4,5 +4,19 @@
  */
 
 $name = 'Димоооооооон *музыка из Бумер*';
+$Dima = 'Дуванов';
+define('AGE',18);
+static $gender = 'Мужской';
 
-var_dump($name);
+$city = 'Мой город: ';
+function my_city() {
+    global $city;
+    $city .= 'Ясиноватая';
+}
+my_city();
+
+echo $name.'<br>'.$Dima.'<br>'.AGE.'<br>'.$gender.'<br>'.$city.'<br>';
+
+unset($name,$Dima,$gender,$city);
+// unset(AGE);
+echo $name.'<br>'.$Dima.'<br>'.AGE.'<br>'.$gender.'<br>'.$city.'<br>';
