e919610be6b47eaac60d2e3a5d83f2880db5b2ad
Yuriy Gurienko <yuriy.gurienko@ya.ru>
2019-02-15 06:04:00 +0300

    #ISSUE - 3# End HOME WORK

--- homework/1/index.php
+++ homework/1/index.php
@@ -1,13 +1,20 @@
 <?php
  // Home work #1
 
- $myName = 'Yuriy';
- $Yuriy = 'Gur';
+$myName = 'Yuriy';
+$Yuriy = 'Gur';
 
- // const FULL_AGE = 33;
- define('FULL_AGE', '33');
- 
- echo "My name is $myName $Yuriy<br>I am " . FULL_AGE . " years";
+// const FULL_AGE = 33;
+define('FULL_AGE', '33');
+
+echo "My name is $myName $Yuriy<br>I am " . FULL_AGE . " years<br>";
+
+static $gender = 'male';
+
+global $city = "Donetsk";
+
+unset ($myName, $Yuriy, $gender, $city);
+
  
  
 
