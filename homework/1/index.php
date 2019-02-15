8a3d1dc436cbc592017208f1218c98290502c10c
Yuriy Gurienko <yuriy.gurienko@ya.ru>
2019-02-15 05:43:35 +0300

    #ISSUE-2# work )

--- homework/1/index.php
+++ homework/1/index.php
@@ -1,2 +1,18 @@
 <?php
- // test pool
+ // Home work #1
+
+ $myName = 'Yuriy';
+ $Yuriy = 'Gur';
+
+ // const FULL_AGE = 33;
+ define('FULL_AGE', '33');
+ 
+ echo "My name is $myName $Yuriy<br>I am " . FULL_AGE . " years";
+ 
+ 
+
+ 
+
+
+
+
