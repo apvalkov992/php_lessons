cc0967c9d328f090edd2214dd02eca4759856864
Arkhi5ov <arkhi5ov@gmail.com>
2019-02-15 19:02:42 +0300

    Home work 1

--- homework/1/index.php
+++ homework/1/index.php
@@ -3,10 +3,20 @@
  *
  */
 
-$name = 'Arkhipov_A';
+$Name = 'Arkhipov_A';
 $a = ((bool) "true";
 $b = ((int) 2019);
 $c = ((float) 2019.2);
 $d = ((string) "Строка");
 $e = null;
-var_dump($name);
+var_dump($Name);
+
+// Домашнее задание урока 1
+
+$Name = `Alexander`;
+$Alexander =`Arkhipov`;
+define("AGE",	"24");
+static $Sex = `male`;
+global $City;
+$City = `Donetsk`;
+unset ($Name, $Alexander, $Sex, $City);
