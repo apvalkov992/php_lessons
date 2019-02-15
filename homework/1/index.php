272239865b8766a2cdf907c198d2b7da516203d5
Denis Zasulskiy <dzasulskiy@gmail.com>
2019-02-15 16:32:12 +0300

    create variables

--- homework/1/index.php
+++ homework/1/index.php
@@ -3,6 +3,22 @@
  *
  */
 
+
 $name = "den";
 
-var_dump($name);
+$den = "Zasulskiy";
+
+const FULL_AGE = 26;
+
+global $city;
+
+static $sex = "male";
+
+global $city;
+
+$city = 'Donetsk';
+
+var_dump($name, $den, $sex, $city);
+
+unset($name, $den, $sex, $city);
+
