a86f74099f215ac21be8b8b37e7ecb76f24e0504
Yuriy Gurienko <yuriy.gurienko@ya.ru>
2019-02-15 18:03:16 +0300

    home work 1

--- homework/1/index.php
+++ homework/1/index.php
@@ -11,9 +11,32 @@ echo "My name is $myName $Yuriy<br>I am " . FULL_AGE . " years<br>";
 
 static $gender = 'male';
 
-global $city = "Donetsk";
+global $city;
+$city = 'Donetsk';
 
 unset ($myName, $Yuriy, $gender, $city);
+
+
+//difference between static and global
+
+function funct() { 
+    static $a; 
+    $a++; 
+    echo " $a";
+
+    global $b;
+    $b++;
+    echo " $b";
+} 
+for ($i = 0; $i++<10;) {
+    funct();
+}
+
+echo "<br>a = $a";
+echo "<br>b = $b";
+     
+
+
 
  
  
