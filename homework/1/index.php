ec1b4ff655f8183f2e1541926f1193b1ad2cfa46
ksenia25085 <blizkayaolga@mail.ru>
2019-02-19 17:52:27 +0300

    #ISSUE-1#создание переменных

--- homework/1/index.php
+++ homework/1/index.php
@@ -2,9 +2,16 @@
 /**
  *
  */
-
-
-
 $name = 'Olga';
-
 var_dump($name);
+
+$null_var=null;
+var_dump($null_var);
+$bool_var=true;
+var_dump($bool_var);
+$int_var=263;
+var_dump($int_var);
+$str_var='Stroka';
+var_dump($str_var);
+$float_var=252.369;
+var_dump($float_var);
