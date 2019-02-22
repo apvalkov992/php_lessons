51e3d79b038d77fb7e3cb47a78d215ebd4a2a95e
mistiqueee <47222544+mistiqueee@users.noreply.github.com>
2019-02-22 22:24:42 +0300

    Update 2.php

--- self-work/2.php
+++ self-work/2.php
@@ -1,46 +1,44 @@
 <?php
-$mons = 6;
-	switch ($mons) {
-		case '1':
+$month = 6;
+	switch ($month) {
+			
+		case 1:
  			echo 'январь';
 			break;
-
-		case '2':
+		case 2:
 			echo 'февраль';
 			break;
-
-		case '3':
+		case 3:
 			echo 'март';
 			break;
-
-		case '4':
+		case 4:
  			echo 'апрель';
  			break;
- 		case '5':
+ 		case 5:
  			echo 'май';
  			break;
- 		case '6':
+ 		case 6:
  			echo 'июнь';
  			break;
- 		case '7':
+ 		case 7:
  			echo 'июль';
  			break;
- 		case '8':
+ 		case 8:
  			echo 'август';
  			break;
- 		case '9':
+ 		case 9:
  			echo 'сентябрь';
  			break;
- 		case '10':
+ 		case 10:
  			echo 'октябрь';
  			break;
- 		case '11:
+ 		case 11:
  			echo 'ноябрь';
  			break;
- 		case '12':
+ 		case 12:
  			echo 'декабрь';
  			break;
 
-	}
+	};
 
 ?>
