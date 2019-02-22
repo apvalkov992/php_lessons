997c219627c22ed204f1a520363346f2c03af2e3
apvalkov <47299078+apvalkov@users.noreply.github.com>
2019-02-22 11:58:35 +0300

    Revert "ДЗ#1"

--- homework/1/index.php
+++ homework/1/index.php
@@ -1,16 +1,8 @@
 <?php
-$my_name = 'Vasiliy';
-$$my_name = 'Guziy';
-define (AGE, 34);
-static $gender = male;
-$town = 'Donetsk';
-global $town;
+/**
+ *
+ */
 
-unset ($my_name, $$my_name, $gender, $town);
+$name = 'My name';
 
-echo "$my_name<br>";
-echo "$$my_name<br>";
-echo AGE;
-echo "<br>$gender<br>";
-echo "$town<br>";
-?>
+var_dump($name);
