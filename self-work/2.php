b660753ea0584abbe0163182d9e65c29027b67ee
mistique <vasiliygvv@yandex.ru>
2019-02-22 21:35:02 +0300

    selfwork

---
+++ self-work/2.php
@@ -0,0 +1,46 @@
+<?php
+$mons = 6;
+	switch ($mons) {
+		case '1':
+ 			echo 'январь';
+			break;
+
+		case '2':
+			echo 'февраль';
+			break;
+
+		case '3':
+			echo 'март';
+			break;
+
+		case '4':
+ 			echo 'апрель';
+ 			break;
+ 		case '5':
+ 			echo 'май';
+ 			break;
+ 		case '6':
+ 			echo 'июнь';
+ 			break;
+ 		case '7':
+ 			echo 'июль';
+ 			break;
+ 		case '8':
+ 			echo 'август';
+ 			break;
+ 		case '9':
+ 			echo 'сентябрь';
+ 			break;
+ 		case '10':
+ 			echo 'октябрь';
+ 			break;
+ 		case '11:
+ 			echo 'ноябрь';
+ 			break;
+ 		case '12':
+ 			echo 'декабрь';
+ 			break;
+
+	}
+
+?>
