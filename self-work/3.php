e385e4cc44264aa0de9fb5e445d920ebf4a6cf70
superaft <ogula.kirill@gmail.com>
2019-02-26 20:30:57 +0300

    Самостоятельная №4

---
+++ self-work/3.php
@@ -0,0 +1,15 @@
+<?php	
+	function search($arr, $to_search){
+		foreach ($arr as $i) {
+			if($i === $to_search){
+				return i;
+			}
+			return null;
+		}
+	}
+
+	$my_arr = [4, 6, 8, 8, 0, 7];
+
+	search(my_arr, 7);
+	var_dump(search(my_arr, 7));
+?>
