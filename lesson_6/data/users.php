dbbdcf020c7b78b055124f841929087ef57c014a
frolov5 <i.slizh.garik@gmail.com>
2019-03-09 14:03:58 +0300

    fix lesson_6

---
+++ lesson_6/data/users.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return [
+    [
+        'email' => 'asdasd@asdas.cc',
+        'password' => '123qwe',
+    ],
+    [
+        'email' => 'test@mail.com',
+        'password' => '123qwe',
+    ],
+    [
+        'email' => 'serv@ddd.cc',
+        'password' => '123qwe',
+    ]
+];
